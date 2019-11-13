f1d8290dbc72754ce37caa8820f9e82dbf2b40f9
SeYearSt <jtiks06@gmail.com>
2019-11-13 21:30:28 +0200

    Update main.py

--- Soultions/lab1/main.py
+++ Soultions/lab1/main.py
@@ -19,6 +19,8 @@ c_3 = 1
 a_31 = -1
 a_32 = 2
 
+c_4 = 1
+
 b1, b2, b3 = 1/6, 4/6, 1/6
 
 
@@ -28,7 +30,8 @@ def main():
   y_0 = 10
   eps = 1e-5
   tau_0 = 0.5
-  eps_M = 1e-6
+  eps_M = 1e-4
+  kf = 0
 
   t = t_0
   y = y_0
@@ -37,70 +40,47 @@ def main():
 
   print("t=", t, "y=", y, 'u(t)=', y_0, '|y-u(t)|=', y - u(t))
 
-  v = y
-  t1 = t
-
-  pr = 1
+  right_call = 0
 
   while True:
 
-    if np.abs(T-t) <= eps_M:
-      print('Максимальна похиюка:', e_max)
-      break
-
- # maybe pr=1
-
-    if pr:
-
-      if t+tau > T:
-        tau = T-t
-
-      v = y
-      t1 = t
-
+    v = y
+    t1 = t
     kf = 0
 
-    if kf == 0:
-      w = y
-      y = v
-      tau = tau/2
-      kf = 1
+    if np.abs(T-t) <= eps_M:
+      break
 
-    if kf == 1:
-      t += tau
-      kf = 2
-
-      if kf in [1, 2]:
-       k1 = f(t, y)
+    if kf==0:
+      k1 = f(t, y)
+      right_call += 1
 
     k2 = f(t + c2 * tau, y + tau * a21 * k1)
+    right_call += 1
     k3 = f(t + c_3 * tau, y + tau * a_31 * k1)
+    right_call += 1
+
+    w = y + tau * k3
     y = y + tau*(b1*k1+b2*k2+b3*k3)
 
-    if kf == 2:
-      E = np.abs(y-w)/((2**p-1)*np.max(1, np.abs(y)))
-      tau_H = 2*tau*min(5, np.max(0.1, 0.9*(eps/E)^(1/(p+1))))
+    E = np.abs(y-w)/max(1, np.abs(y))
+    tauH = tau * min(0.1, 0.9*np.power((eps/E), 1.0/(p+1)))
 
-      if E <= eps:
-        t = t+tau
-        y += (y-w)/(2**p-1)
-        tau = tau_H
+    if E <= eps:
+      error = np.abs(y - u(t))
+      if e_max < error:
+        e_max = error
+      t += tau
+      kf = 0
+    else:
+      y = v
+      t = t1
+      tau = tauH
+      kf = 1
 
-        diff = np.abs(y-u(t))
-
-        print('t =', t, 'u(t)=', u(t), '|y-u(t)|=', diff)
-
-        if e_max < diff:
-          e_max = diff
-
-      else:
-        y = v
-        t = t1
-        tau = tau_H
-        pr = 0
+  print('Максимальна похиюка:', e_max)
+  print('Кількість звертань до правої частини:', right_call)
 
 
 if __name__ == "__main__":
   main()
-
-
